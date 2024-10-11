24de78446752ea6501e673acea49fe89c0c74e5f
Sam35 <sam2010688@gmail.com>
2024-10-11 12:00:39 +0800

    網頁圖示改為本地端

--- js/filemanager.js
+++ js/filemanager.js
@@ -7,25 +7,26 @@ const duplicateButton = document.querySelector('.btn-duplicateUploadFile');
 const template = document.querySelector('.ModaleachUploadFile'); // 模板元素
 
 // 增加一組檔案上傳區塊
-duplicateButton.addEventListener('click', function () {
-  const uploadFiles = document.querySelectorAll('.ModaleachUploadFile');
+if (duplicateButton) {
+  duplicateButton.addEventListener('click', function () {
+    const uploadFiles = document.querySelectorAll('.ModaleachUploadFile');
 
-  if (uploadFiles.length < 3) {
-    const newUpload = template.cloneNode(true); // 複製模板
-    const fileInputs = newUpload.querySelectorAll('input[type="file"]');
-    fileInputs.forEach(input => {
-      input.value = ''; // 確保每次增加的檔案上傳區塊 input 值是空的
-    });
-    uploadContainer.appendChild(newUpload);
+    if (uploadFiles.length < 3) {
+      const newUpload = template.cloneNode(true); // 複製模板
+      const fileInputs = newUpload.querySelectorAll('input[type="file"]');
+      fileInputs.forEach(input => {
+        input.value = ''; // 確保每次增加的檔案上傳區塊 input 值是空的
+      });
+      uploadContainer.appendChild(newUpload);
 
-    // 每次增加區塊後，重新綁定取消按鈕事件
-    updateCancelButtons();
-  }
+      // 每次增加區塊後，重新綁定取消按鈕事件
+      updateCancelButtons();
+    }
 
-  // 控制增加按鈕的顯示與隱藏
-  checkUploadLimit();
-});
-
+    // 控制增加按鈕的顯示與隱藏
+    checkUploadLimit();
+  });
+}
 // 移除檔案上傳區塊
 function updateCancelButtons() {
   const cancelButtons = document.querySelectorAll('.btn-cancelFile');
